d9705ac9dae9c821c26ab9c9a201a3facc4be73c
camblor <alfonsocamblor@gmail.com>
2023-05-13 21:30:29 +0200

    Using rand and ndarray to generate random matrix

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
-use rand::{Rng, distributions::{Standard, Uniform}};
+use rand::{Rng, random, distributions::{Uniform}};
+use ndarray::Array;
 
 fn generate_random_number(min: u32, max: u32) -> u32 {
     let mut rng = rand::thread_rng();
     rng.gen_range(min..=max)
 }
 
+fn test_random_matrix() -> (){
+    let nrows = 5;
+    let ncols = 5;
+
+    let a = Array::from_shape_fn((nrows, ncols), |_| random::<f64>());
+
+    println!("MATRIX {:?}", a);
+}
+
 fn main() {
     fizzbuzz();
     let range = Uniform::from(0..20);
-    let values: Vec<u64> = rand::thread_rng().sample_iter(&range).take(100).collect();
-    println!("Vectorized: {:?}", values);
+
+    let mut a = Vec::new();
+
+    let mut values: Vec<u64> = rand::thread_rng().sample_iter(&range).take(100).collect();
+    a.push(values);
+
+    values = rand::thread_rng().sample_iter(&range).take(100).collect();
+    a.push(values);
+
+
+    //println!("Vectorized: {:?}", values);
+    println!("Matrix: {:?}", a);
+    test_random_matrix();
 }
 
 fn fizzbuzz() -> () {
